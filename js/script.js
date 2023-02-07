ca71b52a14a6afde49a45e39ba47a0a77a66f85f
Maxim Lesev <lesov0iblr@gmail.com>
2023-02-07 14:50:25 +0100

    Commit 4 new

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 
 const btn = document.getElementById('btn');
+const divElem = document.getElementsByClassName('elem')[0];
+
+btn.addEventListener('click', () => {
+    const newElement = document.createElement('div');
+    newElement.classList.add('styleElement');
+    divElem.appendChild(newElement);
+});
+
